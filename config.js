3f3a775a05b24c574a49c6ad439b0ea0a94d55a4
hamzafaisal161 <hamzi161@gmail.com>
2026-02-13 00:52:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/do4kxadtm/video/upload/v1770912220/Hum_Tere_Pyar_Mein_-_Raag.Fm_gro591.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/do4kxadtm/video/upload/v1770925895/Aap_Ki_Tarif_Me_Kya_Kahe_320_Kbps-_SongsPk.com.se_a6ivxf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
